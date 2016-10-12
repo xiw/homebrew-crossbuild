620ddcb6f6280dc1eb59d0761eee2821b9b57cac
Xi Wang <xi@cs.washington.edu>
2016-10-11 23:40:02 -0700

    update to binutils 2.27

--- aarch64-linux-gnu-binutils.rb
+++ aarch64-linux-gnu-binutils.rb
@@ -1,9 +1,9 @@
 class Aarch64LinuxGnuBinutils < Formula
   desc "FSF Binutils for aarch64-linux-gnu development"
   homepage "https://www.gnu.org/software/binutils/binutils.html"
-  url "http://ftpmirror.gnu.org/binutils/binutils-2.26.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/binutils/binutils-2.26.tar.gz"
-  sha256 "9615feddaeedc214d1a1ecd77b6697449c952eab69d79ab2125ea050e944bcc1"
+  url "http://ftpmirror.gnu.org/binutils/binutils-2.27.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz"
+  sha256 "26253bf0f360ceeba1d9ab6965c57c6a48a01a8343382130d1ed47c468a3094f"
 
   def install
     system "./configure", "--prefix=#{prefix}",
