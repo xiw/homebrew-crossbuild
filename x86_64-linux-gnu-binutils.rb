9fea6790e4319bf5da898ae29ef81185241f42dc
Xi Wang <xi@cs.washington.edu>
2017-05-23 23:09:17 -0700

    update to binutils 2.28

--- x86_64-linux-gnu-binutils.rb
+++ x86_64-linux-gnu-binutils.rb
@@ -1,9 +1,9 @@
 class X8664LinuxGnuBinutils < Formula
   desc "FSF Binutils for x86_64-linux-gnu development"
   homepage "https://www.gnu.org/software/binutils/binutils.html"
-  url "http://ftpmirror.gnu.org/binutils/binutils-2.27.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz"
-  sha256 "26253bf0f360ceeba1d9ab6965c57c6a48a01a8343382130d1ed47c468a3094f"
+  url "http://ftpmirror.gnu.org/binutils/binutils-2.28.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz"
+  sha256 "cd717966fc761d840d451dbd58d44e1e5b92949d2073d75b73fccb476d772fcf"
 
   def install
     system "./configure", "--prefix=#{prefix}",
