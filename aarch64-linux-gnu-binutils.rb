8e8342b260edd09ada7876328bcf2ceb97139013
Xi Wang <xi@cs.washington.edu>
2017-07-30 23:05:22 -0700

    update to binutils 2.29

--- aarch64-linux-gnu-binutils.rb
+++ aarch64-linux-gnu-binutils.rb
@@ -1,9 +1,9 @@
 class Aarch64LinuxGnuBinutils < Formula
-  desc "FSF Binutils for aarch64-linux-gnu development"
+  desc "FSF/GNU ld, ar, readelf, etc. for aarch64-linux-gnu development"
   homepage "https://www.gnu.org/software/binutils/binutils.html"
-  url "http://ftpmirror.gnu.org/binutils/binutils-2.28.tar.gz"
-  mirror "https://ftp.gnu.org/gnu/binutils/binutils-2.28.tar.gz"
-  sha256 "cd717966fc761d840d451dbd58d44e1e5b92949d2073d75b73fccb476d772fcf"
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.29.tar.gz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.29.tar.gz"
+  sha256 "172e8c89472cf52712fd23a9f14e9bca6182727fb45b0f8f482652a83d5a11b4"
 
   def install
     system "./configure", "--prefix=#{prefix}",
